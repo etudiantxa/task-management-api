7c434a1562fc8e8b39f81c97ccb52b13aa431294
mor.mangane@vnb-it.fr <mor.mangane@vnb-it.fr>
2026-05-12 14:40:34 +0200

    Ajouter les décorateurs de validation aux DTOs d'authentification

--- src/auth/dtos/login-request.dt.ts
+++ src/auth/dtos/login-request.dt.ts
@@ -1,14 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty } from 'class-validator';
 
 export class LoginRequest {
   @ApiProperty({
     example: 'username',
     required: true,
   })
+  @IsString()
+  @IsNotEmpty()
   username: string;
+  
   @ApiProperty({
     example: 'password',
     required: true,
   })
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
--- src/auth/dtos/register-request.dto.ts
+++ src/auth/dtos/register-request.dto.ts
@@ -1,34 +1,65 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsString,
+  IsEmail,
+  IsOptional,
+  MinLength,
+  MaxLength,
+  Matches,
+} from 'class-validator';
 
 export class RegisterRequestDto {
   @ApiProperty({
     example: 'nom',
     required: true,
   })
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
   nom: string;
+  
   @ApiProperty({
     example: 'prenom',
     required: true,
   })
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
   prenom: string;
+  
   @ApiProperty({
     example: 'email',
     required: false,
   })
+  @IsEmail()
+  @IsOptional()
   email?: string;
+  
   @ApiProperty({
     example: 'password',
     required: true,
   })
+  @IsString()
+  @MinLength(6)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, { 
+    message: 'Password too weak' 
+  })
   password: string;
+  
   @ApiProperty({
     example: 'username',
     required: true,
   })
+  @IsString()
+  @MinLength(2)
+  @MaxLength(50)
   username: string;
+  
   @ApiProperty({
     example: 'photo',
     required: false,
   })
+  @IsOptional()
+  @IsString()
   photo?: string;
 }
