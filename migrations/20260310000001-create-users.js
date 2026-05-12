08bdfe7e8041f8a8e150b70765ea4e8168a59b27
mor.mangane@vnb-it.fr <mor.mangane@vnb-it.fr>
2026-05-12 15:07:00 +0200

    Ajouter la migration manquante pour créer la table users

---
+++ migrations/20260310000001-create-users.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      nom: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      prenom: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      photo: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('users');
+  },
+};
